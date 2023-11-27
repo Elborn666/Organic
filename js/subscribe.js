09589e38dc82fc623184441e947bc0b84064032c
Elborn666 <elborn18@gmail.com>
2023-11-27 08:29:32 +0200

    зробив підписку

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,7 +1,7 @@
-const backdrop = document.querySelector('.backdrop__subscribe')
+const backdrop = document.querySelector('.js-backdrop-subscribe')
 const body = document.querySelector('body')
-const subscribeForm = document.querySelector('.subscribe__form')
-const buttonCloseModal = document.querySelector('.modal__subscribe-button')
+const subscribeForm = document.querySelector('.js-subscribe-form')
+const buttonCloseModal = document.querySelector('.js-button-subscribe')
 
 
 buttonCloseModal.addEventListener('click', onBtnClose)
